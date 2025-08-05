e7aa5abfd6ee6b806649fde0fb5655618b35e825
Aryaman Kumar <aryaman.thakur999888@gmail.com>
2025-08-05 17:04:47 +0530

    Removed unused code

--- unused-code/frontend-utils-lessons/index.ts
+++
@@ -1,9 +0,0 @@
-import { makeLessonScript as HowEconomyWorks } from './howEconomyWorks';
-// Add more imports as you create scripts
-
-export const lessons = {
-  "How Economy Works": HowEconomyWorks,
-  // Add more lessons here
-};
-
-export type LessonName = keyof typeof lessons;
